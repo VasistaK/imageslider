ff2e9087450a049ff685f9006ea16d05f28f18b7
VasistaK <vasistakanderi81@gmail.com>
2024-07-21 21:27:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect, useState} from 'react'
+import { Image_Slider } from './constants'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const App = () => {
+
+const [activeImage,setActiveImage] = useState(0);
+
+
+
+useEffect(()=>{
+  const timer = setInterval(()=>{ (activeImage < Image_Slider.length-1)?setActiveImage(activeImage + 1) : setActiveImage(0)},1000);
+ return ()=> {clearInterval(timer)}
+},[activeImage])
+
+  
+  const nextImage = ()=>{
+     return (activeImage < Image_Slider.length-1)?setActiveImage(activeImage + 1) : setActiveImage(0)
+  }
+ 
+  const previousImage = ()=>{
+    return (activeImage < Image_Slider.length && activeImage > 0  )?setActiveImage(activeImage - 1) : setActiveImage(4)
+  }
+  return (   
+    <div>   
+        <div className='flex border-2 border-cyan-800 justify-evenly mt-2'>
+         <button onClick = {previousImage} className = '' >Previous</button>
+         <div className='flex'> 
+           {Image_Slider.map((x,i)=>{ return <img src={x} alt="wallpaper" key={i} className = {"min-w-[400px] max-h-[400px] " + (activeImage === i ? "block" : "hidden") }/>})}
+           </div>
+           <button onClick={nextImage}>Next</button>
+   </div>
+   <div className='flex justify-center mt-2'>
+    {Image_Slider.map((x,i)=>{return <button onClick={()=>{setActiveImage(i)}} className='mr-2  border-2 border-cyan-400 p-4'>{i}</button>})}</div>
+   </div>
+  )
 }
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const Image_Slider = [
+    "https://cdn.pixabay.com/photo/2024/07/16/12/38/strawberry-8899244_640.jpg",
+    "https://cdn.pixabay.com/photo/2024/07/16/05/31/apple-8898321_640.jpg",
+    "https://cdn.pixabay.com/photo/2024/07/16/16/24/watermelon-8899582_640.jpg",
+    "https://cdn.pixabay.com/photo/2024/07/16/14/46/apple-8899416_640.jpg",
+    "https://cdn.pixabay.com/photo/2024/07/14/16/36/dog-8894814_640.jpg"
+]
